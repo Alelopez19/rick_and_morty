71625bf38a2a309a2fd323982ccfb24672bde266
unknown <alelopez.13.97@gmail.com>
2023-04-10 22:47:37 -0300

    BD Hasta donde me dio

--- back/src/server.js
+++ back/src/server.js
@@ -2,12 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const router = require('./routes/index')
 const morgan = require('morgan');
-const cors = require('cors')
+const cors = require('cors');
+const {conn: sequelize} = require('./DB_connection')
 
 const server = express();
 const PORT = process.env.PORT || 3001;
 
-server.listen(PORT, () => {
+
+server.listen(PORT, async() => {
+    await sequelize.sync({force: true});
     console.log('Server raised in port', PORT)
 });
 server.use(express.json());
